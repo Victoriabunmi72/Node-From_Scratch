6b31320f22bb8950eaa28e5617503cacce5d5e11
Victoriabunmi72 <bhummhie97@gmail.com>
2023-09-02 14:35:47 +0100

    Files

---
+++ first-app/app.js
@@ -0,0 +1,20 @@
+
+//function sayHello(name) {
+   // console.log('Hello ' + name);
+//}
+
+//sayHello('Bunmi');
+
+// Globao Ogject:
+
+//var sayHello = function(){
+
+//}
+
+//window.sayHello();
+
+//console.log(module);
+//......This Is How We work With Modules In Node
+const log = require ('./logger');
+
+log('message');
